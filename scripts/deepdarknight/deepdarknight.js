53e6a0bba5fdef45bbac2d7b09838aeefc16af85
Youhyunwoo <dbgusdn0922@naver.com>
2021-02-13 15:01:51 +0900

    move map and player to scene data

--- scripts/deepdarknight/deepdarknight.js
+++ scripts/deepdarknight/deepdarknight.js
@@ -1,147 +1,26 @@
 import { Engine } from '../engine/core/engine.js';
 import { Game } from '../engine/core/game.js';
 import { Scene } from '../engine/game/scene.js';
-import { GameObject } from '../engine/game/object.js';
-import { World } from './asset/world/world.js';
-import { Map } from './asset/map/map.js';
-import { Player } from './asset/player/player.js';
-import { Stone, Tree } from './asset/data/objects.js';
-import { Vector2 } from '../engine/math/geometry/vector.js';
-import { Area } from '../engine/math/geometry/area.js';
-import { TimeSystem } from './asset/system/timeSystem.js';
-import { UISystem } from '../engine/game/ui/system.js';
-import { InventoryWindow } from './asset/ui/inventory.js';
-import { MixWindow } from './asset/ui/mix.js';
+import { sceneData } from './scene/game.js';
 
 
 
 class DeepDarkNight extends Game {
     init() {
-        this.scene.push(new GameScene());
-    }
-}
+        Scene.deserialize(sceneData).then(scene => {
+            const player = scene
+                .findGameObject('world')
+                .findGameObject('map')
+                .findGameObject('ground')
+                .findGameObject('player');
 
-class GameScene extends Scene {
-    onInitialize() {
-        this.addWorldToScene();
-        this.addPlayerToScene();
-        this.addStoneInWorld();
-        this.addTreeInWorld();
-        this.addTimeSystemToScene();
-        this.addUISystemToScene();
-    }
-
-    addWorldToScene() {
-        this.world = new GameObject('world');
-
-        this.addGameObject(this.world);
-        {
-            // const componentWorld = new World('World');
-
-            // this.world.addComponents(componentWorld);
-
-
-            const map = this.map = new Map('map');
-
-            this.world.addGameObjects(map);
-
-            map.area = new Area(0, 0, 800, 800);
-        }
-    }
-
-    addPlayerToScene() {
-        const map = this.world.findGameObject('map');
-        const layerGround = map.findGameObject('ground');
-
-        const player = this.player = new Player('player');
-
-        layerGround.addGameObjects(player);
-
-        player.area = new Area(100, 100, 20, 16);
-
-        player.addTags('@ground');
-    }
-
-    addStoneInWorld() {
-        const map = this.world.findGameObject('map');
-        const layerGround = map.findGameObject('ground');
-
-        const stoneCount = ~~(Math.random() * 10);
-
-        for (let i = 0; i < stoneCount; i++) {
-            const object = new Stone();
-
-            object.addTags('@ground');
-
-            object.area.x = Math.random() * map.area.width;
-            object.area.y = Math.random() * map.area.height;
-
-            object.init();
-
-            const scale = Math.random() * 0.5 + 0.5;
-
-            object.findComponent('SpriteRenderer').sprite.scale = Vector2.full(scale);
-
-            layerGround.addGameObjects(object);
-        }
-    }
-
-    addTreeInWorld() {
-        const map = this.world.findGameObject('map');
-        const layerGround = map.findGameObject('ground');
-
-        const treeCount = ~~(Math.random() * 5);
-
-        for (let i = 0; i < treeCount; i++) {
-            const object = new Tree();
-
-            object.addTags('@ground');
-
-            object.area.x = Math.random() * map.area.width;
-            object.area.y = Math.random() * map.area.height;
-
-            object.init();
-
-            object.findComponent('SpriteRenderer').sprite.scale = Vector2.full(3);
-
-            layerGround.addGameObjects(object);
-        }
-    }
-
-    addTimeSystemToScene() {
-        this.timeSystem = new GameObject('timeSystem');
-
-        this.addGameObject(this.timeSystem);
-
-        {
-            const componentTimeSystem = new TimeSystem('TimeSystem');
-
-            this.timeSystem.addComponents(componentTimeSystem);
-        }
-    }
-
-    addUISystemToScene() {
-        this.uiSystem = new UISystem('uiSystem');
-
-        this.addGameObject(this.uiSystem);
-
-        {
-            const inventoryWindow = new InventoryWindow();
-
-            inventoryWindow.area = new Area(600, 100, 250, 400);
-            inventoryWindow.visible = false;
-
-            this.uiSystem.addGameObjects(inventoryWindow);
+            scene.player = player;
             
-            const mixWindow = new MixWindow();
-
-            mixWindow.area = new Area(100, 100, 250, 300);
-            mixWindow.visible = false;
-
-            this.uiSystem.addGameObjects(mixWindow);
-        }
+            this.scene.push(scene);
+        });
     }
 }
+
 
 
 const engine = new Engine();
---
+++ scripts/deepdarknight/scene/game.js
@@ -0,0 +1,164 @@
+import { GameObject } from '../../engine/game/object.js';
+import { Area } from '../../engine/math/geometry/area.js';
+
+
+
+export const sceneData = {
+	objects: [
+		{
+			name: 'world',
+			objects: [
+				{
+					name: 'map',
+					objects: [
+						{
+							name: 'ground',
+							objects: [
+								{
+									name: 'player',
+									components: [
+										{
+											module: '/scripts/deepdarknight/asset/player/playerInput.js',
+											type: 'PlayerInput',
+										},
+										{
+											module: '/scripts/engine/graphic/components/spriteRenderer.js',
+											type: 'SpriteRenderer',
+										},
+										{
+											module: '/scripts/engine/graphic/components/animator.js',
+											type: 'Animator',
+										},
+										{
+											module: '/scripts/deepdarknight/asset/character/direction.js',
+											type: 'Direction',
+										},
+										{
+											module: '/scripts/deepdarknight/asset/character/movement.js',
+											type: 'Movement',
+										},
+										{
+											module: '/scripts/deepdarknight/asset/character/gathering.js',
+											type: 'Gathering',
+										},
+										{
+											module: '/scripts/deepdarknight/asset/character/inventory.js',
+											type: 'Inventory',
+										},
+									],
+									objects: [
+										{
+											name: 'state',
+											components: [
+												{
+													module: '/scripts/engine/util/components/state.js',
+													type: 'StateContext',
+												},
+												{
+													module: '/scripts/deepdarknight/asset/player/state/idle.js',
+													type: 'IdleState',
+												},
+												{
+													module: '/scripts/deepdarknight/asset/player/state/move.js',
+													type: 'MoveState',
+												},
+												{
+													module: '/scripts/deepdarknight/asset/player/state/gather.js',
+													type: 'GatherState',
+												}
+											]
+										}
+									],
+									tags: ['@ground'],
+									area: new Area(100, 100, 20, 16),
+								},
+							]
+						},
+						{
+							name: 'sky'
+						}
+					],
+					components: [
+						{
+							module: '/scripts/deepdarknight/asset/map/objectGenerator.js',
+							type: 'ObjectGenerator',
+						},
+						{
+							module: '/scripts/deepdarknight/asset/map/objectSort.js',
+							type: 'ObjectSort',
+						},
+						{
+							module: '/scripts/deepdarknight/asset/map/objectPointer.js',
+							type: 'ObjectPointer',
+						},
+						{
+							module: '/scripts/deepdarknight/asset/map/mapRenderer.js',
+							type: 'MapRenderer',
+						},
+					],
+					tags: [],
+					area: new Area(0, 0, 800, 800),
+				},
+			],
+			components: [],
+			tags: [],
+		},
+		{
+			name: 'timeSystem',
+			components: [
+				{
+					module: '/scripts/deepdarknight/asset/system/timeSystem.js',
+					type: 'TimeSystem'
+				}
+			]
+		},
+		{
+			module: '/scripts/engine/game/ui/system.js',
+			type: 'UISystem',
+			name: 'uiSystem',
+			components: [],
+			objects: [
+				{
+					module: '/scripts/deepdarknight/asset/ui/inventory.js',
+					type: 'InventoryWindow',
+					name: 'inventoryWindow',
+					objects: [
+						{
+							module: '/scripts/engine/game/ui/label.js',
+							type: 'UILabel',
+							area: new Area(10, 10, null, 20),
+							fitContentHorizontal: true,
+							font: '20px 굴림체',
+							text: '인벤토리',
+							backgroundColor: null,
+						}
+					],
+					area: new Area(500, 100, 250, 400),
+					visible: false,
+					backgroundColor: 'white',
+				},
+				{
+					module: '/scripts/deepdarknight/asset/ui/mix.js',
+					type: 'MixWindow',
+					name: 'mixWindow',
+					objects: [
+						{
+							module: '/scripts/engine/game/ui/label.js',
+							type: 'UILabel',
+							area: new Area(10, 10, null, 20),
+							fitContentHorizontal: true,
+							font: '20px 굴림체',
+							text: '조합',
+							backgroundColor: null,
+						}
+					],
+					area: new Area(100, 100, 250, 300),
+					visible: false,
+					backgroundColor: 'white',
+				}
+			]
+		}
+	]
+};
+
+// gameObject -> component: this.getComponent(Type)
