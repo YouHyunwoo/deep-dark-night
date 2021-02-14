ab437c4a41a600c0d9ccf2ace46cccc320beb820
Youhyunwoo <dbgusdn0922@naver.com>
2021-02-14 22:58:54 +0900

    add events and listeners in equipment component

--- scripts/deepdarknight/asset/character/equipment.js
+++ scripts/deepdarknight/asset/character/equipment.js
@@ -5,28 +5,52 @@ import { Statistics } from './statistics.js';
 
 
 export class Equipment extends Component {
+	static get None() { return null; }
+	static get Head() { return 'HEAD'; }
+	static get LeftHand() { return 'LEFTHAND'; }
+	static get RightHand() { return 'RIGHTHAND'; }
+	static get Body() { return 'BODY'; }
+	static get Foot() { return 'FOOT'; }
+
 	constructor(name) {
 		super(name);
-
-		this.stats = null;
 
 		this.head = null;
 		this.leftHand = null;
 		this.rightHand = null;
 		this.body = null;
 		this.foot = null;
+
+		this.listeners = [];
 	}
 
-	onInitialize() {
-		this.stats = this.owner.findComponent(Statistics);
-		this.inventory = this.owner.findComponent(Inventory);
+	addEventListener(listener) {
+		this.listeners.push(listener);
+	}
+
+	removeEventListener(listener) {
+		const index = this.listeners.indexOf(listener);
+
+		if (index >= 0) {
+			this.listeners.splice(index, 1);
+		}
+	}
+
+	notify(event, ...args) {
+		this.listeners.forEach(listener => {
+			if (listener[event]) {
+				listener[event](...args);
+			}
+		});
 	}
 
 	equip(item) {
 		if (this._state === 'initialized') {
+			this.notify('onRequestEquipment', item, this);
+
 			const parts = {
 				'머리': 'head',
-				'손': 'hand',
+				'손': null,
 				'몸': 'body',
 				'발': 'foot',
 			}
@@ -35,42 +59,47 @@ export class Equipment extends Component {
 				if (item.part.endsWith('손')) {
 					if (!this.leftHand) {
 						this.leftHand = item;
+
+						this.notify('onEquip', this.leftHand, 'LEFTHAND', this);
 					}
 					else if (!this.rightHand) {
 						this.rightHand = item;
+
+						this.notify('onEquip', this.rightHand, 'RIGHTHAND', this);
 					}
 					else {
-						this.inventory.addItems({ name: this.leftHand.name, count: 1 });
-						this.stats.apply(this.leftHand.capability, true);
-
+						this.notify('onUnequip', this.leftHand, 'LEFTHAND', this);
 						this.leftHand = this.rightHand;
+						this.notify('onEquip', this.leftHand, 'LEFTHAND', this);
+
+						this.notify('onUnequip', this.rightHand, 'RIGHTHAND', this);
 						this.rightHand = item;
+						this.notify('onEquip', this.rightHand, 'RIGHTHAND', this);
 					}
 				}
 				else {
-					if (this[parts[item.part]]) {
-						this.stats.apply(this[parts[item.part]].capability, true);
+					const equippedItem = this[parts[item.part]];
 
-						this.inventory.addItems({ name: this[parts[item.part]].name, count: 1 });
+					if (equippedItem) {
+						this.notify('onUnequip', equippedItem, parts[item.part].toUpperCase(), this);
 					}
 					
 					this[parts[item.part]] = item;
+
+					this.notify('onEquip', item, parts[item.part].toUpperCase(), this);
 				}
-
-				this.stats.apply(item.capability);
-
-				this.inventory.removeItems({ name: item.name, count: 1 });
 			}
 		}
 	}
 
 	unequip(part) {
-		if (this.state === 'initialized') {
+		if (this._state === 'initialized') {
+			this.notify('onRequestUnequipment', part, this);
+
 			console.assert(part in this);
 
 			if (this[part]) {
-				this.inventory.addItems({ name: this[part], count: 1 });
-				this.stats.apply(this[part].capability, true);
+				this.notify('onUnequip', this[part], part.toUpperCase(), this);
 
 				this[part] = null;
 			}
