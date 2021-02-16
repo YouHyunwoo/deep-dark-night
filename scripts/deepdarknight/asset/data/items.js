27d16c03bf44522b94bb8d943587fe077c4d9d36
Stop <dbgusdn0922@naver.com>
2021-02-16 22:27:19 +0900

    delete item class

--- scripts/deepdarknight/asset/data/items.js
+++ scripts/deepdarknight/asset/data/items.js
@@ -1,14 +1,36 @@
-import { Item } from '../../game/item.js';
 import { spriteStone, spriteTree1, spriteWoodenShoes } from './sprites.js';
 
 
 
 export const items = {
-    '돌': new Item('돌', '돌', spriteStone, '재료', null),
-    '나무': new Item('나무', '나무', spriteTree1, '재료', null),
-    '돌도끼': new Item('돌도끼', '돌과 나무로 만든 도끼', spriteStone, '장비', [['돌', 20], ['나무', 10]], { strength: +8 }),
-    '돌칼': //new Item('돌칼', '돌과 나무로 만든 칼', spriteStone, '장비', [['돌', 10], ['나무', 5]], { strength: +10 }),
-    {
+    '돌': {
+        name: '돌',
+        description: '돌',
+        sprite: spriteStone,
+        type: '재료',
+        part: null,
+        mix: null,
+        capability: null,
+    },
+    '나무': {
+        name: '나무',
+        description: '나무',
+        sprite: spriteTree1,
+        type: '재료',
+        part: null,
+        mix: null,
+        capability: null,
+    },
+    '돌도끼': {
+        name: '돌도끼',
+        description: '돌과 나무로 만든 도끼',
+        sprite: spriteStone,
+        type: '장비',
+        part: '손',
+        mix: [['돌', 20], ['나무', 10]],
+        capability: { strength: +8 },
+    },
+    '돌칼': {
         name: '돌칼',
         description: '돌과 나무로 만든 칼',
         sprite: spriteStone,
